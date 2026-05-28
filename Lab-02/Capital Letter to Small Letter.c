3df80bfcaabc11223980363e0168b8a75c48abbf
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-29 00:39:42 +0600

    Add files via upload

---
+++ Lab-02/Capital Letter to Small Letter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    char ch;
+
+    printf("Enter capital letter: ");
+    scanf("%c", &ch);
+
+    printf("Small letter = %c", tolower(ch));
+
+    return 0;
+}
