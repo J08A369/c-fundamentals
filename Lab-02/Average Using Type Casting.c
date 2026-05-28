2c0d1060d0b2f1473b51152896aa5c0da2d3e6a8
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-28 09:21:49 +0600

    Add files via upload

---
+++ Lab-02/Average Using Type Casting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c;
+    float avg;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    avg = (float)(a + b + c) / 3;
+
+    printf("Average = %.2f", avg);
+
+    return 0;
+}
