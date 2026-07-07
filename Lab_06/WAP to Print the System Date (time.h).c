1a185b7dee57b5be4f91afa0156b6e7849d067dd
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-07-07 08:24:54 +0600

    Add files via upload

---
+++ Lab_06/WAP to Print the System Date (time.h).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t t;
+    struct tm *current;
+
+    time(&t);
+    current = localtime(&t);
+
+    printf("Current Date: %02d/%02d/%04d\n",
+           current->tm_mday,
+           current->tm_mon + 1,
+           current->tm_year + 1900);
+
+    return 0;
+}
