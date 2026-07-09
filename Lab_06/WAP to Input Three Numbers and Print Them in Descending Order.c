6bbd0dc6d420d1a203d8ed9c988b0abf8c01494b
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-07-09 10:10:18 +0600

    Add files via upload

---
+++ Lab_06/WAP to Input Three Numbers and Print Them in Descending Order.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, temp;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a < b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    if(a < c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+
+    if(b < c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("Descending Order: %d %d %d", a, b, c);
+
+    return 0;
+}
