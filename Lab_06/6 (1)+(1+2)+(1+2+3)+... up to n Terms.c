661c04a15cf5ab22cb1417de83809a1a7922e99c
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-07-20 19:47:17 +0600

    Add files via upload

---
+++ Lab_06/6 (1)+(1+2)+(1+2+3)+... up to n Terms.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    int sum = 0, total = 0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = 0;
+        for(j = 1; j <= i; j++)
+        {
+            sum += j;
+        }
+        total += sum;
+    }
+
+    printf("Total = %d", total);
+
+    return 0;
+}
