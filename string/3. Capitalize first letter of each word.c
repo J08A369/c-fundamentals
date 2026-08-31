2403bbbf26f2d65e6827fd55e3f990b3f9f8d971
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-08-31 17:13:55 +0600

    Add files via upload

---
+++ string/3. Capitalize first letter of each word.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    if(str[0] >= 'a' && str[0] <= 'z')
+        str[0] = str[0] - 32;
+
+    for(i = 1; str[i] != '\0'; i++)
+    {
+        if(str[i-1] == ' ' && str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("New string: %s", str);
+
+    return 0;
+}
